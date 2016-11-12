459edf2d35a7aa4feed15cd2b0c27991b2b34a6f
Quake105 <Quake105@users.noreply.github.com>
2016-11-12 13:54:09 -0600

    Add files via upload

--- signup/main.py
+++ signup/main.py
@@ -15,10 +15,143 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        form="""
+        <html><head>
+                <style>
+                    .error {
+                        color: red;
+                    }
+                </style>
+            </head>
+            <body>
+            <h1>Signup</h1>
+                <form method="post">
+                    <table>
+                        <tbody><tr>
+                            <td><label for="username">Username</label></td>
+                            <td>
+                                <input name="username" type="text" value="" required="">
+                                <span class="error"></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="password">Password</label></td>
+                            <td>
+                                <input name="password" type="password" required="">
+                                <span class="error"></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="verify">Verify Password</label></td>
+                            <td>
+                                <input name="verify" type="password" required="">
+                                <span class="error"></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="email">Email (optional)</label></td>
+                            <td>
+                                <input name="email" type="email" value="">
+                                <span class="error"></span>
+                            </td>
+                        </tr>
+                    </tbody></table>
+                    <input type="submit">
+                </form>
+
+        </body></html>
+        """
+
+        self.response.out.write(form)
+
+    def post(self, emailerror="", usererror="", passworderror="", username="", email=""):
+
+        form="""
+        <html><head>
+                <style>
+                    .error {
+                        color: red;
+                    }
+                </style>
+            </head>
+            <body>
+            <h1>Signup</h1>
+                <form method="post">
+                    <table>
+                        <tbody><tr>
+                            <td><label for="username">Username</label></td>
+                            <td>
+                                <input name="username" type="text" value="%(username)s" required="">
+                                <span class="error">%(usererror)s</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="password">Password</label></td>
+                            <td>
+                                <input name="password" type="password" required="">
+                                <span class="error"></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="verify">Verify Password</label></td>
+                            <td>
+                                <input name="verify" type="password" required="">
+                                <span class="error">%(passworderror)s</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label for="email">Email (optional)</label></td>
+                            <td>
+                                <input name="email" type="email" value="%(email)s">
+                                <span class="error">%(emailerror)s</span>
+                            </td>
+                        </tr>
+                    </tbody></table>
+                    <input type="submit">
+                </form>
+
+        </body></html>
+        """
+        password = self.request.get('password')
+        password = escape_htmls(password)
+
+
+        verify = self.request.get('verify')
+        verify = escape_htmls(verify)
+        #def valid_password(password,verify):
+
+        if password == verify:
+            passworderror = ""
+        else:
+            passworderror = "Passwords do not match!"
+
+
+        email = str(self.request.get('email'))
+        email = escape_htmls(email)
+        if re.compile("^[\w.-]+@[\w.-]+\.\w+$").match(email):
+            emailerror = ""
+        else:
+            emailerror = "Email is not valid, please check"
+
+        username = str(self.request.get('username'))
+        username = escape_htmls(username)
+        if re.compile("^\w+\s\w+$").match(username):
+            usererror = "Username is not valid, please do not use space!"
+
+        self.response.out.write(form % {"email": email, "username": username, "usererror": usererror, "passworderror": passworderror,
+                                         "emailerror": emailerror
+                                         })
+
+def escape_htmls(s):
+    return cgi.escape(s, quote=True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
